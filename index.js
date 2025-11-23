059dfb7de1f10817049ba4ccf67dd69a627e7a7a
Lokesh Redekar <redekarlk123@gmail.com>
2025-11-23 23:42:33 +0530

    cors origin allowed for frontend uri

--- index.js
+++ index.js
@@ -17,7 +17,28 @@ connectDB();
 
 // middlewares
 // app.use(cors());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+// app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://ai-pdf-q-a-chatbot.vercel.app"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like Postman, mobile apps)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // routes
